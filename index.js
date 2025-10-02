faec3d9b2d39c7ae7ba1b879fa5f34716a790a9b
[Jesus Martinez] <[jesand13@gmail.com]>
2025-10-02 14:13:17 -0400

    finished 4th problem

--- index.js
+++ index.js
@@ -26,3 +26,10 @@ const multiplesOfThreeOrFive = () => {
     }
 }
 multiplesOfThreeOrFive();
+
+const unitlNum = (n) => {
+    for (let i = 1; i <= n; i++) {
+        console.log(i);
+    }
+}
+unitlNum(5);
