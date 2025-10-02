261751ac48732e2139d225b8b2c7f959fa865975
[Jesus Martinez] <[jesand13@gmail.com]>
2025-10-02 14:47:46 -0400

    finished last problem

--- index.js
+++ index.js
@@ -77,3 +77,16 @@ const betweenTwentyAndForty = (n) => {
     }
 }
 console.log(betweenTwentyAndForty(10));
+
+const largest = (num1, num2, num3) => {
+    if (num1 > num2 && num1 > num3) {
+        return num1;
+    }
+    else if (num2 > num1 && num2 > num3) {
+        return num2;
+    }
+    else {
+        return num3;
+    }
+}
+console.log(largest(30, 22, 17));
