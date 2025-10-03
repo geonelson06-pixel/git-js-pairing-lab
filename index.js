014ac612c717a1362bf0f17c94f7658af74885e8
Gabriel <geonelson06@gmail.com>
2025-10-03 13:39:33 -0400

    Did last 3 questions

--- index.js
+++ index.js
@@ -59,3 +59,25 @@ const largest = (n1,n2,n3)=>{
     return n1>n2?n1>n3?n1:n3:n2>n3?n2:n3
 }
 console.log(largest(90,40,80))
+
+const abs19 = (num) =>{
+    return num>19?num-19:19-num
+}
+
+const revname = (str) =>{
+    return str.split("").reverse().join("")
+}
+
+const alpheba = (str)  => {
+const obj = {
+  a:"b", b:"c", c:"d", d:"e", e:"f", f:"g", g:"h", h:"i", i:"j", j:"k",
+  k:"l", l:"m", m:"n", n:"o", o:"p", p:"q", q:"r", r:"s", s:"t", t:"u",
+  u:"v", v:"w", w:"x", x:"y", y:"z", z:"a"
+};
+
+let bin=''
+    for(let char of str){
+bin+=obj[char]
+}
+    return bin
+}
