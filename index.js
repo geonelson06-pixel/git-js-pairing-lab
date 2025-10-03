450551fff12041bfca141f351f77cb038efcf351
JoshuaDesrosiers <desrosiersjoshua3@gmail.com>
2025-10-03 13:59:46 -0400

    resolved

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
 //Code your solutions in this file
-const fiveToOneHundred = ()=>{
-    for(let x =5;x<=100;x++){
+const fiveToOneHundred = () => {
+    for (let x = 5; x <= 100; x++) {
         console.log(x)
     }
 }
 fiveToOneHundred()
 
-const multiplesOfThree = ()=>{
-    for(let x =0;x<=100;x++){
-        x%3==0?console.log(x):''
+const multiplesOfThree = () => {
+    for (let x = 0; x <= 100; x++) {
+        x % 3 == 0 ? console.log(x) : ''
     }
 
 }
 
 multiplesOfThree()
 
-const multiplesOfThreeOrFive = ()=>{
-    for(let x =0;x<=100;x++){
-        if(x%3 == 0){
+const multiplesOfThreeOrFive = () => {
+    for (let x = 0; x <= 100; x++) {
+        if (x % 3 == 0) {
             console.log(x)
-        } else if(x%5 == 0){
+        } else if (x % 5 == 0) {
             console.log(x)
         }
     }
@@ -27,69 +27,70 @@ const multiplesOfThreeOrFive = ()=>{
 }
 multiplesOfThreeOrFive()
 
-const untilNum = (n)=>{
-    for(let x =0;x<=n;x++){
+const untilNum = (n) => {
+    for (let x = 0; x <= n; x++) {
         console.log(x)
     }
 
 }
 untilNum(5)
 
-const multiply = (n1,n2)=>{
-    return n1*n2
+const multiply = (n1, n2) => {
+    return n1 * n2
 }
 
-const add = (n1,n2)=>{
-    return n1 == n2? n1*n2: n1+n2
+const add = (n1, n2) => {
+    return n1 == n2 ? n1 * n2 : n1 + n2
 }
 
-const isNegative = (n)=>{
-    return n<0 
+const isNegative = (n) => {
+    return n < 0
 }
 
-const triangleArea = (n1,n2)=>{
-    return n1*n2*(1/2) 
+const triangleArea = (n1, n2) => {
+    return n1 * n2 * (1 / 2)
 }
 
-const bt2040 = (n)=>{
-    return (n>20 && n<40)
+const bt2040 = (n) => {
+    return (n > 20 && n < 40)
 }
 
-const largest = (n1,n2,n3)=>{
-    return n1>n2?n1>n3?n1:n3:n2>n3?n2:n3
+const largest = (n1, n2, n3) => {
+    return n1 > n2 ? n1 > n3 ? n1 : n3 : n2 > n3 ? n2 : n3
 }
-console.log(largest(90,40,80))
+console.log(largest(90, 40, 80))
 
-const getTheTime = ()=>{
-const info = new Date()
-return `${info.getHours()}:${info.getMinutes()}:${info.getSeconds()}`
-    
-const getExtension = (arr) =>{
-let ext=''
-let newArr=arr.split('');
-newArr=newArr.reverse()
-for(const char of newArr){char=='.'?break:ext+=char}
-return ext
+const getTheTime = () => {
+    const info = new Date()
+    return `${info.getHours()}:${info.getMinutes()}:${info.getSeconds()}`
 }
-
-const abs19 = (num) =>{
-    return num>19?num-19:19-num
+const getExtension = (arr) => {
+    let ext = ''
+    let newArr = arr.split('');
+    newArr = newArr.reverse()
+    let brk
+    for (const char of newArr) { char == '.' ? brk = true : ext += char; if (brk) { break } }
+    return ext.reverse()
 }
 
-const revname = (str) =>{
+const abs19 = (num) => {
+    return num > 19 ? num - 19 : 19 - num
+}
+
+const revname = (str) => {
     return str.split("").reverse().join("")
 }
 
-const alpheba = (str)  => {
-const obj = {
-  a:"b", b:"c", c:"d", d:"e", e:"f", f:"g", g:"h", h:"i", i:"j", j:"k",
-  k:"l", l:"m", m:"n", n:"o", o:"p", p:"q", q:"r", r:"s", s:"t", t:"u",
-  u:"v", v:"w", w:"x", x:"y", y:"z", z:"a"
-};
+const alpheba = (str) => {
+    const obj = {
+        a: "b", b: "c", c: "d", d: "e", e: "f", f: "g", g: "h", h: "i", i: "j", j: "k",
+        k: "l", l: "m", m: "n", n: "o", o: "p", p: "q", q: "r", r: "s", s: "t", t: "u",
+        u: "v", v: "w", w: "x", x: "y", y: "z", z: "a"
+    };
 
-let bin=''
-    for(let char of str){
-bin+=obj[char]
-}
+    let bin = ''
+    for (let char of str) {
+        bin += obj[char]
+    }
     return bin
 }
